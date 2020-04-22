48d60142f34cc4302420e9a733ee26e204470021
micahswedbergokstate <micah.swedberg@okstate.edu>
2020-04-22 00:31:18 -0500

    Skeleton

---
+++ app/src/main/java/com/example/cs4153pizzaapp/ReviewOrder.kt
@@ -0,0 +1,12 @@
+package com.example.cs4153pizzaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReviewOrder : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_order)
+    }
+}
