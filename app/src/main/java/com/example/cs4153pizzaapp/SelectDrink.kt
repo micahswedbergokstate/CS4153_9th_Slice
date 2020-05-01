75f40175cb9ea30601d88d7c81fc9de222d7d658
micahswedbergokstate <micah.swedberg@okstate.edu>
2020-05-01 15:03:45 -0500

    nonfunctional Side Select and Drink Select Waiting on Order patch before this is functional

--- app/src/main/java/com/example/cs4153pizzaapp/SelectDrink.kt
+++ app/src/main/java/com/example/cs4153pizzaapp/SelectDrink.kt
@@ -25,7 +25,54 @@ class SelectDrink : AppCompatActivity() {
             cokeCount.text = "  " + countItems("Coke") + "  "
             cartText.text = "Cart: $" + Order.total.toString()
         }
-        //etc
+
+        btnAddDietCoke.setOnClickListener {
+            Order.addSide(Side("Diet Coke"))
+            cokeCount.text = "  " + countItems("Diet Coke") + "  "
+            cartText.text = "Cart: $" + Order.total.toString()
+        }
+
+        btnRemoveDietCoke.setOnClickListener {
+            Order.removeSide("Diet Coke")
+            cokeCount.text = "  " + countItems("Diet Coke") + "  "
+            cartText.text = "Cart: $" + Order.total.toString()
+        }
+
+        btnAddSprite.setOnClickListener {
+            Order.addSide(Side("Sprite"))
+            spriteCount.text = "  " + countItems("Sprite") + "  "
+            cartText.text = "Cart: $" + Order.total.toString()
+        }
+
+        btnRemoveSprite.setOnClickListener {
+            Order.removeSide("Sprite")
+            spriteCount.text = "  " + countItems("Sprite") + "  "
+            cartText.text = "Cart: $" + Order.total.toString()
+        }
+
+        btnAddFanta.setOnClickListener {
+            Order.addSide(Side("Fanta"))
+            fantaCount.text = "  " + countItems("Fanta") + "  "
+            cartText.text = "Cart: $" + Order.total.toString()
+        }
+
+        btnRemoveFanta.setOnClickListener {
+            Order.removeSide("Fanta")
+            fantaCount.text = "  " + countItems("Fanta") + "  "
+            cartText.text = "Cart: $" + Order.total.toString()
+        }
+
+        btnAddDrPepper.setOnClickListener {
+            Order.addSide(Side("Dr Pepper"))
+            drPepperCount.text = "  " + countItems("Dr Pepper") + "  "
+            cartText.text = "Cart: $" + Order.total.toString()
+        }
+
+        btnRemoveDrPepper.setOnClickListener {
+            Order.removeSide("Dr Pepper")
+            drPepperCount.text = "  " + countItems("Dr Pepper") + "  "
+            cartText.text = "Cart: $" + Order.total.toString()
+        }
 
         btnReviewOrder.setOnClickListener {
             val intent = Intent(this, ReviewOrder::class.java)
